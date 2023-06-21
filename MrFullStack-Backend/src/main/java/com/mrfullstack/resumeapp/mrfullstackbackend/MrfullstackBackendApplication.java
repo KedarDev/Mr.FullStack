6e1d9091bc7227a8debbd5838f89e99c5c6c8207
KedarDev <kedar.h.dev@gmail.com>
2023-06-21 11:13:41 -0400

    MrFullStack-Backend

---
+++ MrFullStack-Backend/src/main/java/com/mrfullstack/resumeapp/mrfullstackbackend/MrfullstackBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mrfullstack.resumeapp.mrfullstackbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrfullstackBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrfullstackBackendApplication.class, args);
+	}
+
+}
---
+++ MrFullStack-Backend/src/test/java/com/mrfullstack/resumeapp/mrfullstackbackend/MrfullstackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrfullstack.resumeapp.mrfullstackbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrfullstackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
