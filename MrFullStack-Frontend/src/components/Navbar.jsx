1997a5b9a60047f3be3b07242c1056c98275587e
KedarDev <kedar.h.dev@gmail.com>
2024-01-30 20:03:26 -0500

    NavbarFix

--- MrFullStack-Frontend/src/components/Navbar.jsx
+++ MrFullStack-Frontend/src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
       <div className="w-full flex justify-between items-center max-w-7x1 mx-auto">
         <Link
           to="/"
-          className="flex items-center gap-2"
+          className="flex w-[80%] items-center justify-center gap-2"
           onClick={() => {
             setActive(""); // keeps track of where the user is on the page
             window.scrollTo(0, 0);
@@ -27,16 +27,18 @@ const Navbar = () => {
             // onClick={}
             src={darkflash}
             alt="darkflash"
-            className="fixed w-6 h-6 top-1 left-0 object-cover"
+            className=" w-6 h-6 top-1 left-0 object-cover flex justify-center items-center"
           />
+  &nbsp; <span className="flex mx-auto justify-center item">
           <img
             // onClick={}
             src={mflogo}
             alt="mflogo"
-            className="w-full h-8 ml-20 justify-center items-center object-contain  "
+            className=" mx-auto flex w-full h-8 justify-content items-center object-contain"
           />
+          </span>
         </Link>
-        <ul className="list-none hidden sm:flex flex-row gap-10">
+        <ul className="list-none hidden sm:flex mx-auto flex-row gap-6">
           {navLink.map((Link) => (
             <li
               key={Link.id}
@@ -48,12 +50,14 @@ const Navbar = () => {
               <a href={`#${Link.id}`}>{Link.title}</a>
             </li>
           ))}
-          <div className="Button w-28 h-6 relative">
-            <div className="Buttonbox w-28 h-8 left-0 top-0 absolute mix-blend-hard-light bg-black rounded-xl shadow border-4 border-zinc-800" />
-            <div className="Login w-28 h-8 left-[3px] top-[3px] absolute text-center text-white text-[18px] font-normal font-['Inter']">
-              Login
+          <a href="">
+            <div className="Button w-28 h-6 relative right-1 bottom-[3px] ">
+              <div className="Buttonbox w-28 h-8 left-0  absolute mix-blend-hard-light bg-black rounded-xl shadow border-2 border-zinc-800" />
+              <div className="Login w-28 h-8 left-[3px] top-[3px] absolute text-center text-white text-[18px] font-normal font-['Inter']">
+                Login
+              </div>
             </div>
-          </div>
+          </a>
         </ul>
         {/* Hamberger Menu */}
         <div className="sm:hidden flex flex-1 justify-end items-center">
@@ -75,18 +79,23 @@ const Navbar = () => {
                   key={Link.id}
                   className={`${
                     active === Link.title ? "text-[#808080]" : "text-black"
-                  } hover:text-[#FF0000] text-[18px] font-medium cursor-pointer`}
-                  onClick={() => setActive(Link.title)}
+                  } font-poppins text-[16px] font-medium cursor-pointer`}
+                  onClick={() => {
+                    setToggle(!toggle);
+                    setActive(Link.title);
+                  }}
                 >
                   <a href={`#${Link.id}`}>{Link.title}</a>
                 </li>
               ))}
-              <div className="Button w-28 h-6 relative">
-                <div className="Buttonbox w-28 h-8 left-0 top-0 absolute mix-blend-hard-light bg-black rounded-xl shadow border-4 border-zinc-800" />
-                <div className="Login w-28 h-8 left-[3px] top-[3px] absolute text-center text-white text-[18px] font-normal font-['Inter']">
-                  Login
+              <a href="">
+                <div className="Button w-28 h-6 relative">
+                  <div className="Buttonbox w-28 h-8 left-0 top-0 absolute mix-blend-hard-light bg-black rounded-xl shadow border-4 border-zinc-800" />
+                  <div className="Login w-28 h-8 left-[3px] top-[3px] absolute text-center text-white text-[18px] font-normal font-['Inter']">
+                    Login
+                  </div>
                 </div>
-              </div>
+              </a>
             </ul>
           </div>
         </div>
